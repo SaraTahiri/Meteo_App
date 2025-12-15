c0c43537ded71f7931eea6e0d6d2bff339791646
Sara Tahiri <tahirisara911@gmail.com>
2025-12-15 23:02:18 +0100

    Initial commit: Backend Meteo App Spring Boot

---
+++ history-service/src/test/java/com/meteo/historyservice/HistoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meteo.historyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HistoryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ weather-service/src/main/java/com/meteo/weatherservice/WeatherServiceApplication.java
@@ -0,0 +1,15 @@
+package com.meteo.weatherservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class WeatherServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherServiceApplication.class, args);
+    }
+
+}
